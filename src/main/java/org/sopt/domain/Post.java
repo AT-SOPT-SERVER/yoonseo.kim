4bd0f207ec39d9bbbaa8c4aeb093fc8e08880732
yskim6772 <yskim6772@gmail.com>
2025-04-09 21:26:30 +0900

    #2 [feat] : 새로운 게시글 작성 최소 제한 시간 구현

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -1,12 +1,16 @@
 package org.sopt.domain;
 
+import java.time.LocalDateTime;
+
 public class Post {
     private int id;
     private String title;
+    private LocalDateTime createdAt;
 
-    public Post(int id, String title) {
+    public Post(int id, String title, LocalDateTime createdAt) {
         this.id = id;
         this.title = title;
+        this.createdAt = createdAt;
     }
 
     public Post(String title) {
@@ -19,6 +23,10 @@ public class Post {
 
     public String getTitle() {
         return this.title;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
     }
 
     public void setTitle(String title) {
--- src/main/java/org/sopt/service/PostService.java
+++ src/main/java/org/sopt/service/PostService.java
@@ -4,15 +4,28 @@ import org.sopt.domain.Post;
 import org.sopt.repository.PostRepository;
 import org.sopt.util.IdGenerator;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class PostService {
     private PostRepository postRepository = new PostRepository();
 
     public void createPost(String title) {
+        List<Post> postList = postRepository.findAll();
+        if (!postList.isEmpty()) {
+            Post lastPost = postList.get(postList.size() - 1);
+            LocalDateTime lastCreatedAt = lastPost.getCreatedAt();
+            LocalDateTime now = LocalDateTime.now();
+
+            long secondsBetween = Duration.between(lastCreatedAt, now).getSeconds();
+            if (secondsBetween < 180) {
+                throw new IllegalArgumentException("새로운 게시글은 마지막 게시글 작성 이후 3분 뒤에 작성할 수 있습니다.");
+            }
+        }
         validateTitle(title);
         checkDuplicateTitle(title);
-        Post post = new Post(IdGenerator.generatePostId(), title);
+        Post post = new Post(IdGenerator.generatePostId(), title, LocalDateTime.now());
         postRepository.save(post);
     }
 
