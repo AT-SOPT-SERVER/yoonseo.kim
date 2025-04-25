06a14d1b55af8915065b6f5044780622bfc29fde
yskim6772 <yskim6772@gmail.com>
2025-04-25 22:36:30 +0900

    #4 [del] : 불필요한 util 함수 삭제

--- src/main/java/org/sopt/util/TextValidationUtil.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.util;
-
-public class TextValidationUtil {
-    public static int countEmojiRecognizableCharacters(String title) {
-        return title.codePointCount(0, title.length());
-    }
-}
